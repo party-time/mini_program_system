48621c8fc3ce3a04b5997f8bb7227c34f8fe83ad
liuwei@party-time.cn <jushidai123>
2018-01-27 23:20:44 +0800

    提交

---
+++ config-server/src/main/java/cn.partytime/ConfigApplication.java
@@ -0,0 +1,10 @@
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
---
+++ eureka-server/src/main/java/cn.partytime/EurekaApplication.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
