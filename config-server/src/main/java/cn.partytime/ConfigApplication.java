5424b6d48f3a9284c45f71b5682b67f3ee35d778
liuwei@party-time.cn <jushidai123>
2018-01-27 23:45:17 +0800

    初始化

--- config-server/src/main/java/cn.partytime/ConfigApplication.java
+++ config-server/src/main/java/cn.partytime/ConfigApplication.java
@@ -1,4 +1,8 @@
+package cn.partytime;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
--- eureka-server/src/main/java/cn.partytime/EurekaApplication.java
+++ eureka-server/src/main/java/cn.partytime/EurekaApplication.java
@@ -1,3 +1,9 @@
+package cn.partytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
