982f332a0eea398ded552d661d03aec9fee9f195
liuwei@party-time.cn <jushidai123>
2018-01-29 11:38:15 +0800

    修改bug

---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+mvn clean install
+
+# Export the active docker machine IP
+export DOCKER_IP=$(docker-machine ip $(docker-machine active))
+
+# Remove existing containers
+docker-compose stop
+docker-compose rm -f
+
+# Start the discovery service next and wait
+docker-compose up -d eureka-server
+
+while [ -z ${EUREKA_SERVER_READY} ]; do
+  echo "Waiting for eureka server..."
+  if [ "$(curl --silent $DOCKER_IP:9999/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+      EUREKA_SERVER_READY=true;
+  fi
+  sleep 2
+done
+
+# Start the config service first and wait for it to become available
+docker-compose up -d config-server
+
+while [ -z ${CONFIG_SERVER_READY} ]; do
+  echo "Waiting for config server..."
+  if [ "$(curl --silent $DOCKER_IP:9998/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+      CONFIG_SERVER_READY=true;
+  fi
+  sleep 2
+done
+
+
+
+# Start the other containers
+docker-compose up -d
+
+# Attach to the log output of the cluster
+docker-compose logs
