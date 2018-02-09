5dcfdced428a618ea7460d5d78ed7a327cc233c0
liuwei@party-time.cn <jushidai123>
2018-02-09 17:15:22 +0800

    修改bug

---
+++ wait-for-config-server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+while [ -z ${CONFIG_SERVER_READY} ]; do
+  echo "Waiting for config server..."
+  if [ "$(curl --silent $MASTER_IP:9998/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+      CONFIG_SERVER_READY=true;
+  fi
+  sleep 2
+done
---
+++ wait-for-eureka.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+while [ -z ${EUREKA_SERVER_READY} ]; do
+  echo "Waiting for eureka server..."
+  if [ "$(curl --silent $MASTER_IP:9999/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+      EUREKA_SERVER_READY=true;
+  fi
+  sleep 2
+done
