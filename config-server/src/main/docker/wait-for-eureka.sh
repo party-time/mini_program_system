b687473affcf43f519ab88bddac689da42733c6b
liuwei@party-time.cn <jushidai123>
2018-02-27 16:15:29 +0800

    修改bug

--- config-server/src/main/docker/wait-for-eureka.sh
+++ config-server/src/main/docker/wait-for-eureka.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-host="$1"
-shift
 cmd="$@"
 
 while [ -z ${EUREKA_SERVER_READY} ]; do
   echo "Waiting for eureka server..."
-  if [ "$(curl --silent $host:9999/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+  if [ "$(curl --silent $MASTER_IP:9999/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
       EUREKA_SERVER_READY=true;
   fi
   sleep 2
--- wait-for-config-server.sh
+++ wait-for-config-server.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+host="$1"
+shift
 cmd="$@"
 
 while [ -z ${CONFIG_SERVER_READY} ]; do
   echo "Waiting for config server..."
-  if [ "$(curl --silent $MASTER_IP:9998/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+  if [ "$(curl --silent $host:9998/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
       CONFIG_SERVER_READY=true;
   fi
   sleep 2
